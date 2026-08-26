55c04897eec2ad73fc3971c2cd367de97e90ab6c
Hyunjun Kim <rmekdma@gmail.com>
2026-08-26 15:52:40 +0900

    chore(release): prepare v4.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = "neth.iecal.curbox"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "4.0.2"
+        versionCode = 6
+        versionName = "4.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "app_name", "Curbox")
 
